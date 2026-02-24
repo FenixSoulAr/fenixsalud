46d2e539a2e088176826d49e7bd17e0b5c614e41
FenixSoulAr <fenixsoular@gmail.com>
2026-02-24 17:54:29 -0300

    feat: add Android platform (Capacitor) and update config

---
+++ android/app/src/main/java/com/fenixsoular/myhealthhub/MainActivity.java
@@ -0,0 +1,5 @@
+package com.fenixsoular.myhealthhub;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.fenixsoular.myhealthhub',
+  appName: 'My Health Hub',
+  webDir: 'dist'
+};
+
+export default config;
